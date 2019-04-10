9bb86dff583d7a629a7778f177ac9422df5d62fe
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-09 21:12:16 -0500

    imported Route and Switch from react-router-dom and implemented all routes for the app navigation

--- tech-store/src/App.js
+++ tech-store/src/App.js
@@ -5,18 +5,29 @@ import { FaUsb } from "react-icons/fa";
 import styled from "styled-components";
 
 import AboutPage from "./pages/AboutPage";
-import cartPage from "./pages/CartPage";
+import CartPage from "./pages/CartPage";
 import ContactPage from "./pages/ContactPage";
 import Default from "./pages/Default";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import SingleProductPage from "./pages/SingleProductPage";
 
+import { Route, Switch } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
       <h1>Tech-Store <FaUsb/>
       <Button>Styled Component</Button>
+      <Switch>
+        <Route path="/" exact component={HomePage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+        <Route path="/contact" component={ContactPage}></Route>
+        <Route path="/products" component={ProductsPage}></Route>
+        <Route path="/product/:id" exact component={SingleProductPage}></Route>
+        <Route path="/cart" component={CartPage}></Route>
+        <Route component={Default}></Route>
+      </Switch>
       </h1>
     );
   }
