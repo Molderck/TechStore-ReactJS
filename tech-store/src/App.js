4fe6c4d37c66c02c1077293c1f5aefa3682fe2da
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-09 20:16:08 -0500

    Imported boostrap and App.css styles

--- tech-store/src/App.js
+++ tech-store/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div>Tech-Store</div>
+      <h1>Tech-Store</h1>
     );
   }
 }
