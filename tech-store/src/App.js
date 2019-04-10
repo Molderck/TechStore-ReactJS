8ce7f040c99936b25c935df6219267d171d1e6a4
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-09 20:25:14 -0500

    Imported react icons and checked how to use one icon for testing

--- tech-store/src/App.js
+++ tech-store/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaUsb } from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
-      <h1>Tech-Store</h1>
+      <h1>Tech-Store <FaUsb/></h1>
     );
   }
 }
