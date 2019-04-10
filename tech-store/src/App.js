e5c43b0bcae6ba1d96954a07a5af5343d0af97ee
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-09 20:34:28 -0500

    Imported styled components and used them to create a Button for testing

--- tech-store/src/App.js
+++ tech-store/src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaUsb } from "react-icons/fa";
+import styled from "styled-components";
 
 class App extends Component {
   render() {
     return (
-      <h1>Tech-Store <FaUsb/></h1>
+      <h1>Tech-Store <FaUsb/>
+      <Button>Styled Component</Button>
+      </h1>
     );
   }
 }
+
+const color = "#f15025";
+const Button = styled.button`
+  color: white;
+  background: ${color};
+` 
 
 export default App;
