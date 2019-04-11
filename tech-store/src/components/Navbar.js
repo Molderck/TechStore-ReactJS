82baf17b2d9629f5efed328966306f09a6152d1e
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-11 12:46:01 -0500

    Added content for navbar using the ProductConsumer and set up NavWrapper styles to be implemented

--- tech-store/src/components/Navbar.js
+++ tech-store/src/components/Navbar.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { FaBars, FaCartPlus } from "react-icons/fa";
+import styled from "styled-components";
+import { ProductConsumer } from "../context/Context";
+import Logo from "../images/logo.svg";
 
 export default function Navbar() {
   return (
-    <React.Fragment>
-        <h1>Navbar</h1>
-    </React.Fragment>
+    <ProductConsumer>
+      {value => {
+        const { cartItems, handleSidebar, handleCart } = value;
+        return <NavWrapper>
+          <div className="nav-center">
+            <FaBars className="nav-icons" onClick={handleSidebar} />
+            <img src={Logo} alt="TechStore Logo" />
+            <div className="nav-cart">
+              <FaCartPlus className="nav-icon" onClick={handleCart} />
+              <div className="cart-items">
+                {cartItems}
+              </div>
+            </div>
+          </div>
+        </NavWrapper>
+      }}
+    </ProductConsumer>
   )
 }
+
+const NavWrapper = styled.nav`
+
+`  
