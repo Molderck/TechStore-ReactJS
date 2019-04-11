165d61520204f1c57bda6ed39360e537bfe76864
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-11 12:54:11 -0500

    Added styles to NavWrapper component to display the navbar properly

--- tech-store/src/components/Navbar.js
+++ tech-store/src/components/Navbar.js
@@ -27,5 +27,38 @@ export default function Navbar() {
 }
 
 const NavWrapper = styled.nav`
+  position: -webkit-sticky
+  position: sticky;
+  top: 0;
+  width: 100%;
+  padding: 1rem 1.5rem;
+  background: var(--mainGrey);
+  border-bottom: 3px solid var(--primaryColor);
+  .nav-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
 
+  .nav-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+
+  .nav-cart {
+    position: relative;
+  }
+
+  .cart-items {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    font-size: 0.85rem;
+    position: absolute;
+    top: -8px;
+    right: -8px;
+    padding: 0 5px;
+    border-radius: 50%;
+  }
 `  
