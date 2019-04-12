454c421ffefc88186e5e23b8145f676822dfabb8
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-12 12:56:06 -0500

    Created SideWrapper and added Context value prop to create all links for the navigation inside the Sidebar component

--- tech-store/src/components/Sidebar.js
+++ tech-store/src/components/Sidebar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context/Context";
+
 
 export default function Sidebar() {
   return (
-    <React.Fragment>
-        <h1>Sidebar</h1>
-    </React.Fragment>
+    <ProductConsumer>
+      {value => {
+        const { links, sidebarOpen, handleSidebar } = value;
+        return (
+          <SideWrapper>
+            <ul>
+                {links.map(link => {
+                  return (
+                    <li key={link.id}>
+                    <Link to={link.path} className="sidebar-link" onClick={handleSidebar}>{link.text}</Link>
+                    </li>
+                  )
+                })}
+            </ul>
+          </SideWrapper>
+        )
+      }}
+    </ProductConsumer>
   )
 }
+
+const SideWrapper = styled.nav`
+
+`
