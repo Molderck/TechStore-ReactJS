284a63cb9b10b9e7fbea4568c4f084a2fd247382
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-12 20:02:15 -0500

    Added CSS for the Sidecart component

--- tech-store/src/components/Sidecart.js
+++ tech-store/src/components/Sidecart.js
@@ -18,5 +18,17 @@ export default function Sidecart() {
 }
 
 const CartWrapper = styled.div`
-
-`
+  position: fixed;
+  top: 60px;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGrey);
+  z-index: 1;
+  transform: ${props => (props.show ? "translateX(0)" : "translateX(100%)")};
+  border-left: 4px solid var(--primaryColor);
+  transition: var(--mainTransition);
+  @media (min-width: 576px){
+    width: 20rem;
+  }
+`;
