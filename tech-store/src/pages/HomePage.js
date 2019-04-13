c69f17348dc3cdd108cfbc72da853c8870b199bd
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-12 20:39:45 -0500

    Added CSS for Hero component to display properly the title and the background image - The image prop value is assigned by default with defaultProps for the cases where img props is not defined

--- tech-store/src/pages/HomePage.js
+++ tech-store/src/pages/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
-    <Hero title="Awesome Gadgets">
+    <Hero title="Awesome Gadgets" max={true} >
       <Link to="/products">Our Products</Link>
     </Hero>
   )
