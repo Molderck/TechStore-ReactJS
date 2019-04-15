f0cd0310a06cc9a880b45a07b1666cfdef956659
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-15 13:43:30 -0500

    Implemented Default page with background, styles and a Link to index

--- tech-store/src/pages/Default.js
+++ tech-store/src/pages/Default.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import defaultBcg from '../images/defaultBcg.jpeg';
+import { Link } from 'react-router-dom';
 
 export default function Default() {
   return (
     <React.Fragment>
-        <h1>Default</h1>
+      <Hero img={defaultBcg} title="404" max="true">
+        <h2 className="text-uppercase">Page not Found</h2>
+        <Link to="/" className="main-link" style={{ marginTop: "2rem" }}>Return Home</Link>
+      </Hero>
     </React.Fragment>
   )
 }
