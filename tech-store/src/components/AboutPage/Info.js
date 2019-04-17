87c6e76ae00a76d3703ca668620faa488f510f0f
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-16 20:40:18 -0500

    Added HTML for Info component to display the image that goes below the AboutPage component

--- tech-store/src/components/AboutPage/Info.js
+++ tech-store/src/components/AboutPage/Info.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import aboutBcg from '../../images/aboutBcg.jpeg';
 
 export default function Info() {
     return (
-        <div>
-            Info
-            <Title/>
-        </div>
+        <section className="py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 my-3">
+                        <img src={aboutBcg} className="img-fluid img-thumbnail" alt="About" style={{ background: "var(--darkGrey)" }}></img>
+                    </div>
+                    <div className="col-10 mx-auto col-md-6 my-3">
+                    </div>
+                </div>
+            </div>
+            <Title title="About Us" />
+        </section>
     )
 }
