a057eedcbc3d5fae60ee5e21a354ae41639bcd8e
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-16 20:28:42 -0500

    Added TitleWrapper styled component and all HTML to the Title Component

--- tech-store/src/components/AboutPage/Title.js
+++ tech-store/src/components/AboutPage/Title.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Title() {
+export default function Title({ title, center }) {
     return (
-        <div>
-            Title
-        </div>
+        <TitleWrapper className="row" center={center}>
+            <div className="col">
+                <h2 className="text-title">{title}</h2>
+                <div className="title-underline" />
+            </div>
+        </TitleWrapper>
     )
 }
+
+const TitleWrapper = styled.div`
+
+` 
