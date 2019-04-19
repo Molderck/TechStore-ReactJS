dff3ac0b904cc13084e7a50bef55b374a9229607
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-19 18:07:42 -0500

    Added HTML to Footer component in order to display Copyright text and all social media links with its respective icons

--- tech-store/src/components/Footer.js
+++ tech-store/src/components/Footer.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import { ProductConsumer } from "../context/Context";
 
 export default function Footer() {
   return (
-    <React.Fragment>
-        <h1>Footer</h1>
-    </React.Fragment>
+    <ProductConsumer>
+      {value => {
+        return <FooterWrapper>
+          <div className="container py-3">
+            <div className="row">
+              <div className="col-md-6">
+                <p className="text-capitalize">Copyright &copy; TechStore {new Date().getFullYear()} all rights reserved</p>
+              </div>
+              <div className="col-md-6">
+                {value.socialLinks.map(item => <a href={item.url} key={item.id}>{item.icon}</a>)}
+              </div>
+            </div>
+          </div>
+        </FooterWrapper>
+      }}
+    </ProductConsumer>
   )
 }
+
+const FooterWrapper = styled.footer`
+
+`
