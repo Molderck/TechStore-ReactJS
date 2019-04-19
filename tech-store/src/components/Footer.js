0347e1c64d424dc92393a0a9e377f99eedf4696f
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-19 18:15:46 -0500

    Add css to FooterWrapper styled component to display all social media icons properly

--- tech-store/src/components/Footer.js
+++ tech-store/src/components/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
               <div className="col-md-6">
                 <p className="text-capitalize">Copyright &copy; TechStore {new Date().getFullYear()} all rights reserved</p>
               </div>
-              <div className="col-md-6">
+              <div className="col-md-6 d-flex justify-content-around">
                 {value.socialLinks.map(item => <a href={item.url} key={item.id}>{item.icon}</a>)}
               </div>
             </div>
@@ -24,5 +24,15 @@ export default function Footer() {
 }
 
 const FooterWrapper = styled.footer`
-
+  background: var(--darkGrey);
+  color: var(--mainWhite);
+  .icon {
+    font-size: 1.5rem;
+    color: var(--mainWhite);
+    transition: var(--mainTransition);
+  }
+  .icon:hover {
+    color: var(--primaryColor);
+    cursor: pointer;
+  }
 `
