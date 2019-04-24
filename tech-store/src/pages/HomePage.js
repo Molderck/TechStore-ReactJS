d3f473a99ac67b2c06e17bf06b95cf7593190a41
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-04-23 22:34:46 -0500

    Imported Services and Featured components and used them in the HomePage component

--- tech-store/src/pages/HomePage.js
+++ tech-store/src/pages/HomePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
+import Services from "../components/HomePage/Services";
+import Featured from "../components/HomePage/Featured";
 
 export default function HomePage() {
   return (
-    <Hero title="Awesome Gadgets" max={true} >
-      <Link to="/products" className="main-link" style={{ margin: "2rem"}}>Our Products</Link>
-    </Hero>
+    <>
+      <Hero title="Awesome Gadgets" max={true} >
+        <Link to="/products" className="main-link" style={{ margin: "2rem" }}>Our Products</Link>
+      </Hero>
+      <Services />
+      <Featured />
+    </>
   )
 }
