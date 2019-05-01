7e9461debea7e407ec36835281eaf3a6d6de2903
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-05-01 12:55:54 -0500

    Imported Product, Title, ProductConsumer and added HTML to display all products inside the featuredProducts state of context

--- tech-store/src/components/HomePage/Featured.js
+++ tech-store/src/components/HomePage/Featured.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Product from "../Product";
+import { Link } from "react-router-dom";
+import Title from "../Title";
+import { ProductConsumer } from "../../context/Context";
 
 export default function Featured() {
     return (
-        <div>
-            Featured
-    </div>
+        <section className="py-5">
+            <div className="container">
+                <Title title="Featured Product" center="true" />
+                <div className="row">
+                    <ProductConsumer>
+                        {value => {
+                            const { featuredProducts } = value;
+                            return featuredProducts.map(product => (<Product key={product.id} product={product} />))
+                        }}
+                    </ProductConsumer>
+                </div>
+            </div>
+        </section>
     )
 }
