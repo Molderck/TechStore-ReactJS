769497e1803683dba2f66acd3ef12009adc0c6b8
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-05-01 13:42:41 -0500

    Added CSS to ProductWrapper styled component for the card, img and icons

--- tech-store/src/components/Product.js
+++ tech-store/src/components/Product.js
@@ -23,7 +23,7 @@ export default function Product({ product }) {
                             </div>
                             <div className="card-body d-flex justify-content-between">
                                 <p className="mb-0">{product.title}</p>
-                                <p className="mb-0 text-main">{product.price}</p>
+                                <p className="mb-0 text-main">$ {product.price}</p>
                             </div>
                         </div>
                     </ProductWrapper>
@@ -34,5 +34,54 @@ export default function Product({ product }) {
 }
 
 const ProductWrapper = styled.div`
-    
+   .card {
+       box-shadow: 5px 5px 5px 0px rgba(0,0,0,0.3);
+       transition: var(--mainTransition);
+       height: 100%;
+   } 
+
+   .card:hover {
+       box-shadow: 7px 10px 5px 0px rgba(0,0,0,0.5);
+       cursor: pointer;
+   }
+
+   .card-img-top {
+       transition: var(--mainTransition);
+   }
+
+   .card:hover .card-img-top {
+       transform: scale(1.15);
+       opacity: 0.2;
+   }
+
+   .img-container {
+       position: relative
+   }
+
+   .product-icons {
+       transition: var(--mainTransition);
+       position: absolute;
+       top: 50%;
+       left: 50%;
+       transform: translate(-50%, -50%);
+   }
+
+   .icon {
+       font-size: 2.5rem;
+       margin: 1rem;
+       padding: 0.5rem;
+       color: var(--primaryColor);
+       background: var(--mainBlack);
+       border-radius: 0.5rem;
+   }
+
+   .card:hover .product-icons {
+       opacity: 1;
+   }
+
+   .card-body {
+       font-weight: bold;
+       letter-spacing: 2px;
+       text-transform: uppercase;
+   }
 `
