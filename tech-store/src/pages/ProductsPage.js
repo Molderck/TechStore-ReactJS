35ac508bc1dab289f44118fb7a142c26873efe4d
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-05-29 22:30:52 -0500

    Imported background for ProductsPage and imported Product component

--- tech-store/src/pages/ProductsPage.js
+++ tech-store/src/pages/ProductsPage.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Products from "../components/ProductsPage/Products";
+import Hero from "../components/Hero";
+import productBcg from "../images/productsBcg.jpeg";
 
 export default function ProductsPage() {
   return (
-    <React.Fragment>
-        <h1>ProductsPage</h1>
-    </React.Fragment>
-  )
+    <>
+      <Hero img={productBcg} />
+      <Products />
+    </>
+  );
 }
