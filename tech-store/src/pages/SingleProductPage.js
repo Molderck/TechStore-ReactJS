918593b6984ba49796618f8dfcc05d14ad43966e
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-01 22:35:26 -0500

    Added the info of the selected Product to SingleProductPage and added two buttons, one for adding the actual product to the cart and another link to see all products

--- tech-store/src/pages/SingleProductPage.js
+++ tech-store/src/pages/SingleProductPage.js
@@ -8,6 +8,66 @@ export default function SingleProductPage() {
   return (
     <>
       <Hero img={singleProductImg} title="Single Product" />
+      <ProductConsumer>
+        {value => {
+          const { singleProduct, addToCart, loading } = value;
+          if (loading) {
+            return <h1>Product Loading ...</h1>;
+          }
+
+          const {
+            company,
+            description,
+            id,
+            price,
+            title,
+            image
+          } = singleProduct;
+          return (
+            <section className="py-5">
+              <div className="cantaine">
+                <div className="row">
+                  <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                    <img
+                      src={`../${image}`}
+                      alt="SingleProduct"
+                      className="img-fluid"
+                    />
+                  </div>
+                  <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                    <h5 className="text-title mb-4">Model: {title}</h5>
+                    <h5 className="text-capitalize text-muted mb-4">
+                      Company: {company}
+                    </h5>
+                    <h5 className="text-main text-capitalize mb-4">
+                      Price: ${price}
+                    </h5>
+                    <p className="text-capitalize text-title mb-3">
+                      Some Info About Product:{" "}
+                    </p>
+                    <p>{description}</p>
+                    <button
+                      type="button"
+                      className="main-link"
+                      style={{ margin: "0.75rem" }}
+                      onClick={() => addToCart(id)}
+                    >
+                      Add To Cart
+                    </button>
+                    <Link
+                      to="/products"
+                      className="main-link"
+                      style={{ margin: "0.75rem" }}
+                    >
+                      Back To Products
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </section>
+          );
+        }}
+      </ProductConsumer>
     </>
   );
 }
