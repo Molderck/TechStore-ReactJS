a699991e4b4a45f4e4fb36b9a1bde680c781b69b
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-04 23:03:48 -0500

    CartList component displays a message if the cart is empty or gets the cartItem and all the functions for each product

--- tech-store/src/components/CartPage/CartList.js
+++ tech-store/src/components/CartPage/CartList.js
@@ -1,11 +1,39 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { ProductConsumer } from "../../context/Context";
 
 export default function CartList() {
   return (
-    <div>
-      CartList
-      <CartItem />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <ProductConsumer>
+            {value => {
+              const { cart, increment, decrement, removeItem } = value;
+              if (cart.length === 0) {
+                return (
+                  <h1 className="text-title text-center my-4">
+                    Your Cart Is Currenty Empty
+                  </h1>
+                );
+              }
+              return (
+                <>
+                  {cart.map(item => (
+                    <CartItem
+                      key={item.id}
+                      CartItem={item}
+                      increment={increment}
+                      decrement={decrement}
+                      removeItem={removeItem}
+                    />
+                  ))}
+                </>
+              );
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
     </div>
   );
 }
