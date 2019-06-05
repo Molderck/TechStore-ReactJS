a558908fddc3fd5967a0d8fe2402b3e245bcb425
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-04 23:10:58 -0500

    Destructuring all data from CartItem prop to display respective image of product in the cart

--- tech-store/src/components/CartPage/CartItem.js
+++ tech-store/src/components/CartPage/CartItem.js
@@ -1,5 +1,23 @@
 import React from "react";
+import {
+  FaTrash,
+  FaChevronCircleUp,
+  FaChevronCircleDown
+} from "react-icons/fa";
 
-export default function CartItem() {
-  return <div>CartItem</div>;
+export default function CartItem({
+  CartItem,
+  increment,
+  decrement,
+  removeItem
+}) {
+  const { id, title, price, count, total, image } = CartItem;
+
+  return (
+    <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
+      <div className="col mx-auto col-lg-2 pb-2">
+        <img src={image} alt="Product" width="60" className="img-fluid" />
+      </div>
+    </div>
+  );
 }
