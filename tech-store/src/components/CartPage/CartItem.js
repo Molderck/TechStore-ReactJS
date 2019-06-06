3a3a148dd37f53d090ac1c473a978ea7eb9a12c3
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-05 21:36:40 -0500

    Added more info to the CartItem Component and passed the decrement, increment and removeItem methods to the button icons

--- tech-store/src/components/CartPage/CartItem.js
+++ tech-store/src/components/CartPage/CartItem.js
@@ -15,8 +15,47 @@ export default function CartItem({
 
   return (
     <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
-      <div className="col mx-auto col-lg-2 pb-2">
+      <div className="col-10 mx-auto col-lg-2 pb-2">
         <img src={image} alt="Product" width="60" className="img-fluid" />
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none ">Product:</span>
+        {title}
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <span className="d-lg-none ">Price: $ </span>
+        {price}
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <FaChevronCircleDown
+              className="cart-icon text-primary"
+              onClick={() => {
+                decrement(id);
+              }}
+            />
+            <span className="text-title text-muted mx-3">{count}</span>
+            <FaChevronCircleUp
+              className="cart-icon text-primary"
+              onClick={() => increment(id)}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <FaTrash
+          className="text-danger cart-icon"
+          onClick={() => removeItem(id)}
+        />
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <strong className="text-muted">Item Total: $ {total}</strong>
       </div>
     </div>
   );
