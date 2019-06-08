38f0d1cbc7a5bbb3de43cd5d0f04ccca81870a31
Sebastian Ortiz <sebastiansalazaro@hotmail.com>
2019-06-07 22:25:53 -0500

    Implemented handleChange method for all inputs and checkbox

--- tech-store/src/context/Context.js
+++ tech-store/src/context/Context.js
@@ -228,7 +228,19 @@ class ProductProvider extends Component {
     );
   };
 
-  handleChange = event => {};
+  handleChange = event => {
+    const name = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    this.setState(
+      {
+        [name]: value
+      },
+      this.sortData
+    );
+  };
   sortData = () => {};
 
   render() {
